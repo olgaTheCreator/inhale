e18811bca609d69e5b6d96afbe0256dea423179c
OJB <curylo.olga@gmail.com>
2022-03-08 17:24:40 +0000

    continuing work on slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,8 +4,20 @@ import "./sliderStyle.css";
 export const Slider = () => {
   return (
     <div className="slider-wrapper">
-      <div className="slider-track"></div>
-      <div className="slider-thumb"></div>
+      <div
+        className="slider-track"
+        onClick={(e) => console.log(e.clientX, e.clientY)}
+        onMouseMove={(e) => {
+          let posX = 0;
+          posX = e.clientX;
+          console.log({ posX });
+        }}
+      ></div>
+      <div
+        className="slider-thumb"
+        onMouseDown={(e) => console.log(e.clientX, e.clientY)}
+        style={{ top: `{posX}` }}
+      ></div>
     </div>
   );
 };
